a829005f7fa59c6a06ae0210259f14156d502152
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2025-03-05 12:29:21 +0600

    vignere (#6)

--- dist/Cipher.d.ts
+++ dist/Cipher.d.ts
@@ -1,10 +1,12 @@
 import { Caesar } from "./ciphers/Ceaser.js"
 import { Atbash } from "./ciphers/Atbash.js"
 import { Playfair } from "./ciphers/Playfair.js"
+import { Vigenere } from "./ciphers/Vigenere.js"
 export declare abstract class Cipher {
   static Caesar: typeof Caesar
   static Atbash: typeof Atbash
   static Playfair: typeof Playfair
+  static Vigenere: typeof Vigenere
   abstract encrypt(text: string): string
   abstract decrypt(text: string): string
 }
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { Cipher } from "./Cipher.js"
 import { Caesar } from "./ciphers/Ceaser.js"
 import { Atbash } from "./ciphers/Atbash.js"
 import { Playfair } from "./ciphers/Playfair.js"
+import { Vigenere } from "./ciphers/Vigenere.js"
 
 Cipher.Caesar = Caesar
 Cipher.Atbash = Atbash
 Cipher.Playfair = Playfair
+Cipher.Vigenere = Vigenere
 
 export { Cipher }
