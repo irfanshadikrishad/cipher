58af1604ac5afa252e046e1784911b333e976604
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2025-03-08 21:50:34 +0600

    the alphabet cipher

--- src/Cipher.ts
+++ src/Cipher.ts
@@ -2,12 +2,14 @@ import { Caesar } from "./ciphers/Caesar.js"
 import { Atbash } from "./ciphers/Atbash.js"
 import { Playfair } from "./ciphers/Playfair.js"
 import { Vigenere } from "./ciphers/Vigenere.js"
+import { Alphabet } from "./ciphers/Alphabet.js"
 
 export abstract class Cipher {
   static Caesar: typeof Caesar
   static Atbash: typeof Atbash
   static Playfair: typeof Playfair
   static Vigenere: typeof Vigenere
+  static Alphabet: typeof Alphabet
 
   abstract encrypt(text: string): string
   abstract decrypt(text: string): string
