9d8bc464cf146351ee861e577f1ebf4a64c71f37
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2025-07-21 08:44:03 +0600

    fix: prettier (github) issue (#96)

--- tests/adfgvx.test.ts
+++ tests/adfgvx.test.ts
@@ -1,10 +1,10 @@
 import { Cipher } from '../src/index'
 
 test('ADFGVX', () => {
-	const adfgvx = new Cipher.ADFGVX(null, 'CIPHER')
-	const plaintext = 'youseebutyoudonotobserve'
-	const encrypted = adfgvx.encrypt(plaintext)
-	const decrypted = adfgvx.decrypt(encrypted)
+  const adfgvx = new Cipher.ADFGVX(null, 'CIPHER')
+  const plaintext = 'youseebutyoudonotobserve'
+  const encrypted = adfgvx.encrypt(plaintext)
+  const decrypted = adfgvx.decrypt(encrypted)
 
-	expect(decrypted).toBe(plaintext.replace(/[^A-Za-z0-9]/g, '').toUpperCase())
+  expect(decrypted).toBe(plaintext.replace(/[^A-Za-z0-9]/g, '').toUpperCase())
 })
--- tests/des.test.ts
+++ tests/des.test.ts
@@ -1,10 +1,10 @@
 import { Cipher } from '../src/index'
 
 test('DES', () => {
-	const des = new Cipher.DES('12345678')
-	const plaintext = 'Hello, World!'
-	const encrypt = des.encrypt(plaintext)
-	const decrypt = des.decrypt(encrypt)
+  const des = new Cipher.DES('12345678')
+  const plaintext = 'Hello, World!'
+  const encrypt = des.encrypt(plaintext)
+  const decrypt = des.decrypt(encrypt)
 
-	expect(decrypt).toBe(plaintext)
+  expect(decrypt).toBe(plaintext)
 })
